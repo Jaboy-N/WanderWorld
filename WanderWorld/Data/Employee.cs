8a29ade6042ed051c8036695be85cf703fd8bfd8
Mark Flagg <markflagg@miplan.co.za>
2025-05-19 15:02:59 +0200

    Add project files.

---
+++ WanderWorld/Data/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WanderWorld.Data
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string? LastName { get; set; }
+
+        [Required]
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ WanderWorld/Data/EmployeeMangerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WanderWorld.Data
+{
+    public class EmployeeMangerContext : DbContext
+    {
+        public EmployeeMangerContext 
+            (DbContextOptions<EmployeeMangerContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; } = default!;
+    }
+}
